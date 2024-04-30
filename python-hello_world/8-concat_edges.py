d9a68da3edf4175a2b0411baf9e9983a79322bc1
aallm004 <abigailrosea10@gmail.com>
2024-04-30 00:33:03 +0000

    forgot to add str to locations

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-str = [39:66] + [121:125] + [1:6]
+str = str[39:66] + str[121:125] + str[1:6]
 print(str)
