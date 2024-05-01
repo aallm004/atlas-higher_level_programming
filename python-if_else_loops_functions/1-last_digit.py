e5ab232ae83a0c20092cb6cf5abc2e8a95550075
aallm004 <abigailrosea10@gmail.com>
2024-05-01 18:55:59 +0000

    added negative sign

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -6,5 +6,7 @@ if number > 5:
     print(f"Last digit of {number} is {lastDigit} and is greater than 5")
 elif number == 0:
     print(f"Last digit of {number} is {lastDigit} and is 0")
+elif number < 0:
+	print(f"Last digit of {number} is -{lastDigit} and is less than 6 and not 0")
 else:
     print(f"Last digit of {number} is {lastDigit} and is less than 6 and not 0")
