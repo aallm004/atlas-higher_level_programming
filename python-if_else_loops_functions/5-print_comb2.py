c91f16bf09770d7dcd53301dda4d6356d7ec8066
aallm004 <abigailrosea10@gmail.com>
2024-05-01 21:31:04 +0000

    made condition for 98

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
-for number in range(0, 99):
-    print("{:02}".format(number), end=', ')
+for number in range(0, 98):
+    if number == 98:
+        print("{}".format(number))
+    else:
+        print("{:02}".format(number), end=', ')
