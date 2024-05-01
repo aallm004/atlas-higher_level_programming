339841876fbb42097fbe5278454fbe64f3f98f08
aallm004 <abigailrosea10@gmail.com>
2024-05-01 23:45:10 +0000

    trying to get the last digit negative

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -2,9 +2,13 @@
 import random
 number = random.randint(-10000, 10000)
 lastDigit = number % 10
+
 if number > 5:
     print(f"Last digit of {number} is {lastDigit} and is greater than 5")
 elif number == 0:
     print(f"Last digit of {number} is {lastDigit} and is 0")
+elif number < 0:
+    lastDigit = number % -10
+    print(f"Last digit of {number} is {lastDigit} and is less than 6 and not 0")
 else:
     print(f"Last digit of {number} is {lastDigit} and is less than 6 and not 0")
