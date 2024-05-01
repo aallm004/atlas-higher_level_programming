e412cb0710cfba24f30df4fa09baa0e66354e47d
aallm004 <abigailrosea10@gmail.com>
2024-05-01 19:25:03 +0000

    changed else statement

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -4,7 +4,7 @@ number = random.randint(-10000, 10000)
 if number < 0:
 	new_num = -(abs(number) % 10)
 else:
-	number % 10
+	new_num = abs(number) % 10
 
 if number > 5:
     print(f"Last digit of {number} is {lastDigit} and is greater than 5")
