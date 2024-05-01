36191111a53606edbcfdde13d9ae543cf6a9be72
aallm004 <abigailrosea10@gmail.com>
2024-05-01 19:08:07 +0000

    new method to get last digit negative if negative

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 import random
 def last_digit(num):
-    return num - (10 * number(num / 10))
+    last_digit_unsigned = abs(num) % 10
+	return -last_digit_unsigned if (num < 0) else last_digit_unsigned
+
 if number > 5:
     print(f"Last digit of {number} is {lastDigit} and is greater than 5")
 elif number == 0:
