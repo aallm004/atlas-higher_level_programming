e6559ae2265314e97facedaa6c19ebdee55f6325
aallm004 <abigailrosea10@gmail.com>
2024-05-01 21:06:07 +0000

    added brackets with q and e to be omitted

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 for i in range(ord('a'), ord('z')+1):
-    if i != 101 and i != 113:
-    print("{}".format(chr(i)), end='')
+    if chr(i) not in ['q', 'e']:
+        print("{}".format(chr(i)), end='')
     
