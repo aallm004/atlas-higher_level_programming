7aba1d08c1f3fdf7bb45a0f5e3d073967d0aa913
aallm004 <abigailrosea10@gmail.com>
2024-05-02 19:41:20 +0000

    added new line after 89

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -2,6 +2,6 @@
 for a in range (0, 9):
     for b in range (a + 1, 10):
         if a == 8 and b == 9:
-            print("{}{}".format(a,b), end="")
+            print("{}{}".format(a,b))
         else:
             print("{}{}".format(a,b), end=", ")
