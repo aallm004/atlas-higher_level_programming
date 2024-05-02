b30d08edb22cf91e45c966c950d7b8c71ec658b2
aallm004 <abigailrosea10@gmail.com>
2024-05-02 19:20:22 +0000

    accidental space removed

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def uppercase(str):
     for x in str:
-        if ord(x) >= ord('a') and ord(x) <= ord ('z'):
+        if ord(x) >= ord('a') and ord(x) <= ord('z'):
             x = chr(ord(x) - 32)
             print("{}".format(x), end="")
     print("")
