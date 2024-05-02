474d5a79aa0faefea8128096e79b9efde3d12749
aallm004 <abigailrosea10@gmail.com>
2024-05-02 21:24:41 +0000

    added space after return

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -4,4 +4,4 @@ def print_last_digit(number):
         print("Traceback (most recent call last):")
     else:
         print(str(number)[-1], end="")
-    return(str(number)[-1])
+    return (str(number)[-1])
