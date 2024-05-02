c46187690737f9ec2e7319d9369e66c37e0812fa
aallm004 <abigailrosea10@gmail.com>
2024-05-02 21:14:29 +0000

    first commit for

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,1 +1,7 @@
 #!/usr/bin/python3
+def print_last_digit(number):
+    if number == "abcdefghijklmnopqrstuvwxyz":
+        print("Error")
+    else:
+        print(number[-1], end="")
+        return(number[-1])
