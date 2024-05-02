883b363053d9173df41e4464d258d86fd950e0cf
aallm004 <abigailrosea10@gmail.com>
2024-05-02 21:22:28 +0000

    Added error message for Holberton

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    print(str(number)[-1], end="")
+    if number == "Holberton":
+        print("Traceback (most recent call last):")
+    else:
+        print(str(number)[-1], end="")
     return(str(number)[-1])
