3bf576159ffb847232b896ed493df7696185c17e
aallm004 <abigailrosea10@gmail.com>
2024-05-02 19:42:44 +0000

    pycode comma correction

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-for a in range (0, 9):
-    for b in range (a + 1, 10):
+for a in range(0, 9):
+    for b in range(a + 1, 10):
         if a == 8 and b == 9:
-            print("{}{}".format(a,b))
+            print("{}{}".format(a, b))
         else:
-            print("{}{}".format(a,b), end=", ")
+            print("{}{}".format(a, b), end=", ")
