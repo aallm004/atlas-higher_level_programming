982552f4cae65736087d63ae3ce90a91ff05cd8d
aallm004 <abigailrosea10@gmail.com>
2024-05-02 16:51:53 +0000

    erased second condition of number being less than zero

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -3,7 +3,7 @@ import random
 number = random.randint(-10000, 10000)
 if number < 0:
     lastDigit = number % 10
-elif number > 0:
+else:
     lastDigit = number % -10
 
 print(f"Last digit of {number} is {lastDigit}", end="")
@@ -12,7 +12,5 @@ if number > 5:
     print(f" and is greater than 5")
 elif number == 0:
     print(f" and is 0")
-elif number < 0:
-    print(f" and is less than 6 and not 0")
 else:
     print(f" and is less than 6 and not 0")
