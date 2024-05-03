da89be00e55f6010ffd67ab38f3b671d385db51a
aallm004 <abigailrosea10@gmail.com>
2024-05-03 20:37:12 +0000

    added missing files for task zero

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
-def add(a,b):
+from add_0 import add
+if __name__ == "__main__":
     a = 1
     b = 2
-
     result = add(a, b)
-
-    print(f"{a} + {b} = {result}")
-    
+print("{} + {} = {}".format(a, b, result))
