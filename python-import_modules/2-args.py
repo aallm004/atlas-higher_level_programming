8e8f350a2153bc61f2cc638693a9808c7ae4635b
aallm004 <abigailrosea10@gmail.com>
2024-05-03 22:27:42 +0000

    first commit task two

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+import sys
+if __name__ == "__main__":
+    if len(sys.argv) == 1:
+        print("0 arguments.")
+    elif len(sys.argv) == 2:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(len(sys.argv)))
+for index in range(1, len(sys.argv) + 1):
+        print("{}: {}".format(index, sys.argv[index]))
