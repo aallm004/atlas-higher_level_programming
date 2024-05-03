ce8c17c19d5d56701889021ad6ed554c6663e4f0
aallm004 <abigailrosea10@gmail.com>
2024-05-03 22:33:29 +0000

    psycode correction

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -8,4 +8,4 @@ if __name__ == "__main__":
     else:
         print("{} arguments:".format(len(sys.argv) - 1))
 for index in range(1, len(sys.argv)):
-        print("{}: {}".format(index, sys.argv[index]))
+    print("{}: {}".format(index, sys.argv[index]))
