19bd08b82614d61a60e013823d4728c8302eb9c7
aallm004 <abigailrosea10@gmail.com>
2024-05-04 20:53:55 +0000

    first commit task 8

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+def multiple_returns(sentence):
+    length = len(sentence)
+    if length == 0:
+        first_char = "None"
+    else:
+        first_char = sentence[0]
+    return tuple((length, first_char))
