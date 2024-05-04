f8514e660e46eb3eecc771a72ed8e7a50e1dad0a
aallm004 <abigailrosea10@gmail.com>
2024-05-04 21:05:20 +0000

    first commit task 9

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+def max_integer(my_list=[]):
+    if my_list == 0:
+        return "None"
+    res = 0
+    for i in my_list:
+        if i >= res:
+            res = i
+    return res
+
+
