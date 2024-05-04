f9c75ed1f0e8d0bd95d1357598184adc973818eb
aallm004 <abigailrosea10@gmail.com>
2024-05-04 00:18:42 +0000

    first commit task 4

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -2,5 +2,5 @@
 import hidden_4
 if __name__ == "__main__":
     for d in dir(hidden_4):
-        if d[0] != "__":
-            print("{}".format(hidden_4))
+        if d[0:2] != "__":
+            print("{}".format(d))
