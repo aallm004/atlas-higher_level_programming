c2ca695f2ea701c74920c827f75ccb594d767181
aallm004 <abigailrosea10@gmail.com>
2024-05-05 20:54:51 +0000

    try/finally task 8

--- python-more_data_structures/8-simple_delete.py
+++ python-more_data_structures/8-simple_delete.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 def simple_delete(a_dictionary, key=""):
-    i = False
-    for k in a_dictionary:
-        if k == key:
-            i = True
-        else:
-            del a_dictionary[key]
-            return(a_dictionary)
+    try:
+        a_dictionary.pop(key)
+    finally:
+        return a_dictionary 
