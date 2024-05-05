23389137e7208884e117de281f200374eebefda8
aallm004 <abigailrosea10@gmail.com>
2024-05-05 17:43:19 +0000

    first commit task 1

--- python-more_data_structures/2-uniq_add.py
+++ python-more_data_structures/2-uniq_add.py
@@ -1,1 +1,7 @@
 #!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    replace_function = lambda x: replace if x == search else x
+
+    new_list = list(map(replace_function, my_list))
+
+    return new_list
