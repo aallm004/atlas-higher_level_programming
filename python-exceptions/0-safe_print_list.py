d07198bedbf6c83adc43f0cc0a09cf64b0fa53a9
aallm004 <abigailrosea10@gmail.com>
2024-05-06 21:13:09 +0000

    pycodestyle errors corrected

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    
+
     try:
         print_el = 0
         for element in my_list:
-                if print_el < x:
-                    print(element, end='')
-                    print_el += 1
+            if print_el < x:
+                print(element, end='')
+                print_el += 1
 
     except IndexError:
-            print(f"An error occurred: {element}")
+        print(f"An error occurred: {element}")
     print()
     return print_el
