3627ba77d33a0b4251e8ff6881b39e93d155e0ad
aallm004 <abigailrosea10@gmail.com>
2024-05-06 22:07:17 +0000

    replaced my_list[i] with i

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -3,8 +3,9 @@ def safe_print_list_integers(my_list=[], x=0):
     print_int = 0
     for i in range(x):
         try:
-            print("{:d}".format(my_list[i]), end="")
+            print("{:d}".format(i), end="")
             print_int += 1
         except (ValueError, TypeError):
-            print()
-            return print_int
+            pass
+        print()
+        return print_int
