e9722bf69f1f624899e094e2efa50cb6c32b31db
aallm004 <abigailrosea10@gmail.com>
2024-05-07 19:20:16 +0000

    result = None

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
-    result = 0
+    result = None
     try:
         result = a / b
-
         return result
     except ZeroDivisionError:
         return None
     finally:
-        if result is not None:
-            print("Inside result: {}".format(result))
+        print("Inside result: {}".format(result))
