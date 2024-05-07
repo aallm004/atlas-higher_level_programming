af1e3b5aa9ea059ffbf3a805ec8c91e891e0cc69
aallm004 <abigailrosea10@gmail.com>
2024-05-07 19:44:48 +0000

    moved result out of for loop

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -13,5 +13,5 @@ def list_division(my_list_1, my_list_2, list_length):
             print("out of range")
         finally:
             final.append(result)
-        return final
+    return final
     
