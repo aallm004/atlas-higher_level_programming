b1abc7d2509ddd7013287b475fa3ff39ead18869
aallm004 <abigailrosea10@gmail.com>
2024-05-08 01:51:11 +0000

    this works, support

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+def roman_to_int(roman_string):
+    roman = {"IV": 4, "IX": 9, "XL":40, "XC":90, "CD":400, "CM":900, "I":1, "V":5, "X":10, "L":50, "C":100, "D":500, "M":1000}
+    result = 0
+
+    for i in roman:
+        while i in roman_string:
+            result += roman.get(i)
+            roman_string = roman_string.replace(i, "", 1)
+    return result
