41f6858c4c041f69c4892207116bd03970addc9f
aallm004 <abigailrosea10@gmail.com>
2024-05-08 01:57:02 +0000

    line too long, no longer too long

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    roman = {"IV": 4, "IX": 9, "XL": 40, "XC": 90, "CD": 400, "CM": 900, "I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+    roman = {"IV": 4, "IX": 9, "XL": 40,
+             "XC": 90, "CD": 400, "CM": 900,
+             "I": 1, "V": 5, "X": 10, "L": 50,
+             "C": 100, "D": 500, "M": 1000}
     result = 0
     if isinstance(roman_string, str):
         for i in roman:
