366c4ffe40b9031f2ba8e7476fe5652e1e5d1c83
aallm004 <abigailrosea10@gmail.com>
2024-05-09 21:33:13 +0000

    does space change things?

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -4,7 +4,7 @@
 
 class Square:
     """Square is defined"""
-    def __init__(self, size=0, position=(0,0)):
+    def __init__(self, size=0, position=(0, 0)):
         self.__size = size
         self.__position = position
 
