8ad6aca6786cd319e45af2f3ae6546b64183a836
aallm004 <abigailrosea10@gmail.com>
2024-05-09 18:17:45 +0000

    first commit task 2

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+"""New class called Square is defined"""
+
+
+class Square:
+    """Square is defined"""
+    def __init__(self, size=0):
+        self.__size = size
+        """A private instance attribute: size"""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
