d5da46fe982021075392a4b50ab3b91ed2d36d66
aallm004 <abigailrosea10@gmail.com>
2024-05-09 20:10:38 +0000

    pycodestyle whitespace

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -31,9 +31,10 @@ class Square:
         self.__size = value
 
     def my_print(self):
+        """prints rectangles with # and new line if nothing"""
         if self.__size == 0:
             print()
             return
-    
+
         for _ in range(self.__size):
             print("#" * self.__size)
