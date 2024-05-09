0eec9fa2e76975be87de20a438b7801479ddfca7
aallm004 <abigailrosea10@gmail.com>
2024-05-09 19:40:59 +0000

    commit 1 task 4

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -1,1 +1,26 @@
 #!/usr/bin/python3
+"""New class called Square is defined"""
+
+
+class Square:
+    """Square is defined"""
+    def __init__(self, size=0):
+        self.__size = size
+        """A private instance attribute: size"""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+    def area(self):
+        """Returns current square area"""
+        return self.__size ** 2
+    
+    @property
+    def size(self):
+        """Used to retrieve size"""
+        return self.__size
+    
+    @size.setter
+    def size(self, value):
+        self.__size = value
