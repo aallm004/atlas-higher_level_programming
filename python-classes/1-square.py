a2c28025c677e97f376bac7b589168169ef4bcc4
aallm004 <abigailrosea10@gmail.com>
2024-05-09 18:00:18 +0000

    Pycodestyle correction

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -3,7 +3,8 @@
 
 
 class Square:
-    """Square is defined and has a private instance attribute, size"""
+    """Square is defined and has a 
+    private instance attribute, size"""
     def __init__(self, size):
         self.__size = size
         """A private instance attribute by the name of size with no type/value verification"""
