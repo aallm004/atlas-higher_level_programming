46a98ed6fea1fb5a54320fcd92069dde8a2700c4
aallm004 <abigailrosea10@gmail.com>
2024-05-09 17:59:39 +0000

    commit one task one

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+"""New class called Square is defined"""
+
+
+class Square:
+    """Square is defined and has a private instance attribute, size"""
+    def __init__(self, size):
+        self.__size = size
+        """A private instance attribute by the name of size with no type/value verification"""
