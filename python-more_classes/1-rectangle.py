1f708da45faf9a6afd79f380296b503348e0146d
aallm004 <abigailrosea10@gmail.com>
2024-05-10 22:59:56 +0000

    added documentation

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -10,10 +10,12 @@ class Rectangle:
 
     @property
     def width(self):
-         return self.__width
+        """used to retrieve width"""
+        return self.__width
         
     @width.setter
     def width(self, value):
+        """used to set width"""
         self.__width = value
     if not isinstance(value, int):
         raise TypeError("height must be an integer")
@@ -22,10 +24,12 @@ class Rectangle:
 
     @property
     def height(self):
+        """used to retrieve height"""
         return self.__height
 
     @height.setter
     def height(self, value):
+        """used to set height"""
         self.__height = value
     if not isinstance(value, int):
         raise TypeError("height must be an integer")
