bcaeda06c88387be7fc2f36af182865e8bcf12bf
aallm004 <abigailrosea10@gmail.com>
2024-05-10 23:06:55 +0000

    new placement for value definition

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -16,7 +16,7 @@ class Rectangle:
     @width.setter
     def width(self, value):
         """used to set width"""
-        self.__width = value
+    self.__width = value
     if not isinstance(value, int):
         raise TypeError("width must be an integer")
     if width < 0:
@@ -30,13 +30,8 @@ class Rectangle:
     @height.setter
     def height(self, value):
         """used to set height"""
-        self.__height = value
+    self.__height = value
     if not isinstance(value, int):
         raise TypeError("height must be an integer")
     if height < 0:
         raise ValueError("height must be >= 0")
-        
-        
-
-        
-
