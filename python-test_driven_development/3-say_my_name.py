0fe132040d6857c86722925c28b4d0a17ccdd318
aallm004 <abigailrosea10@gmail.com>
2024-05-12 19:04:55 +0000

    ahhh

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python3
+"""
+This function prints a message.
+
+Functions:
+    say_my_name(first_name, last_name): Prints "My name is <first name> <last name>".
+"""
+
 def say_my_name(first_name, last_name=""):
 
 
