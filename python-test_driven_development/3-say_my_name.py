8770f0e21c21d8b9c043990b8ff16b1b680d330f
aallm004 <abigailrosea10@gmail.com>
2024-05-12 18:34:25 +0000

    added type

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -16,9 +16,9 @@ def say_my_name(first_name, last_name=""):
         first_name and last_name    
     """
 
-    if first_name is not str:
+    if type(first_name) is not str:
         raise TypeError("first_name must be a string")
-    if last_name is not str:
+    if type(last_name) is not str:
         raise TypeError("last_name must be a string")
 
     print(("My name is {:s}{:s}".format(first_name, last_name)))
