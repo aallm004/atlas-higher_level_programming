113bc45536d5c7b9f07070ecbada777332ce4979
aallm004 <abigailrosea10@gmail.com>
2024-05-12 19:25:05 +0000

    more indentation

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -27,5 +27,5 @@ def print_square(size):
     if size < 0:
         raise ValueError("size must be > 0")
 
-for i in range(size):
-    print("#" * size)
+    for i in range(size):
+        print("#" * size)
