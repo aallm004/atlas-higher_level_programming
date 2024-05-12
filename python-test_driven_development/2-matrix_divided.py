b179b11a4db41e1c77f665bdc75f7ba63b96c899
aallm004 <abigailrosea10@gmail.com>
2024-05-12 16:20:39 +0000

    forgotten g

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -43,7 +43,7 @@ def matrix_divided(matrix, div):
             raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
 
         if length is 0:
-            lenth = len(row)
+            length = len(row)
         elif len(row) is not length:
             raise TypeError("Each row of the matrix must have the same size")
 
