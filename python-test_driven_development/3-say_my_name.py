46d3b9bc255458a27b68b3ff0586bbb4b7ca9d88
aallm004 <abigailrosea10@gmail.com>
2024-05-12 19:08:40 +0000

    where is the extra space

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-This function prints a message.
+This function prints a line
 
 Functions:
     say_my_name(first_name, last_name): Prints "My name is <first name> <last name>".
@@ -30,5 +30,5 @@ def say_my_name(first_name, last_name=""):
         """Checking to see if the last_name is a string"""
         raise TypeError("last_name must be a string")
 
-    print(("My name is {:s}{:s}".format(first_name, last_name)))
+    print(("My name is {:s} {:s}".format(first_name, last_name)))
     """Printing first_name and last_name"""
