a33d4fa85921e6f172d32f572b047e6513e53b5f
aallm004 <abigailrosea10@gmail.com>
2024-05-12 18:46:31 +0000

    documentation

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -17,8 +17,11 @@ def say_my_name(first_name, last_name=""):
     """
 
     if not isinstance(first_name, str):
+        """Checking to see if the first_name is a string"""
         raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):
+        """Checking to see if the last_name is a string"""
         raise TypeError("last_name must be a string")
 
     print(("My name is {:s}{:s}".format(first_name, last_name)))
+    """Printing first_name and last_name"""
