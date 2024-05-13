d3de560d185624e7ccc8725ff398dae35a04d46f
aallm004 <abigailrosea10@gmail.com>
2024-05-13 21:59:03 +0000

    counter for numofinstances

--- python-more_classes/5-rectangle.py
+++ python-more_classes/5-rectangle.py
@@ -3,6 +3,9 @@
 
 
 class Rectangle:
+    """Class Rectangle which defines a rectangle"""
+    number_of_instances = 0
+    
     """Initialize Rectangle data"""
     def __init__(self, width=0, height=0):
         self.width = width
