2ba5bd6da72ff0b8ddcbb9b037e09be7f17175bc
aallm004 <abigailrosea10@gmail.com>
2024-05-13 21:49:35 +0000

    accidental =

--- python-more_classes/3-rectangle.py
+++ python-more_classes/3-rectangle.py
@@ -52,7 +52,7 @@ class Rectangle:
 
     def __str__(self):
         if self.width == 0 or self.height == 0:
-            return = ""
+            return ""
         else:
             string = ""
             for x in range(self.height):
