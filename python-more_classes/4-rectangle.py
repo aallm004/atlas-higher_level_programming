31e956c9c93a2ba0846ee5b48abc025e8a7f3071
aallm004 <abigailrosea10@gmail.com>
2024-05-13 21:53:03 +0000

    def repr

--- python-more_classes/4-rectangle.py
+++ python-more_classes/4-rectangle.py
@@ -1,1 +1,64 @@
 #!/usr/bin/python3
+"""New class called that defines a Rectangle"""
+
+
+class Rectangle:
+    """Initialize Rectangle data"""
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """used to retrieve width"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """used to set width"""
+        self._width = value
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = int(value)
+
+    @property
+    def height(self):
+        """used to retrieve height"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """used to set height"""
+        self._height = value
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = int(value)
+
+    def area(self):
+        return self.width * self.height
+
+    def perimeter(self):
+        if self.width == 0 or self.height == 0:
+            return 0
+        else:
+            return 2 * (self._width + self._height)
+
+    def __str__(self):
+        if self.width == 0 or self.height == 0:
+            return ""
+        else:
+            string = ""
+            for x in range(self.height):
+                string += "#" * self.width
+                if x < self.height - 1:
+                    string += "\n"
+            return string
+    
+    def __repr__(self):
+        return "Rectangle({}, {})".format(self.width, self.height)
