76cc70a1862a45d8001f2c0d77ccd858dad26f51
aallm004 <abigailrosea10@gmail.com>
2024-05-14 22:31:11 +0000

    too many blanks

--- python-more_classes/8-rectangle.py
+++ python-more_classes/8-rectangle.py
@@ -53,7 +53,7 @@ class Rectangle:
             return 0
         else:
             return 2 * (self._width + self._height)
-    
+
     @staticmethod
     def bigger_or_equal(rect_1, rect_2):
         if not isinstance(rect_1, Rectangle):
@@ -64,7 +64,6 @@ class Rectangle:
             return rect_1
         else:
             return rect_2
-
 
     def __str__(self):
         if self._width == 0 or self._height == 0:
