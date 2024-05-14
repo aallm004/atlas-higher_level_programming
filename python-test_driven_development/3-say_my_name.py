3ebd97a0823f6cecff15fd8b0c6f09fb5e181b8a
aallm004 <abigailrosea10@gmail.com>
2024-05-14 20:12:18 +0000

    line too long

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -3,7 +3,8 @@
 This function prints a line
 
 Functions:
-    say_my_name(first_name, last_name): Prints "My name is <first name> <last name>".
+    say_my_name(first_name, last_name): 
+    Prints "My name is <first name> <last name>".
 """
 
 
