2ac855fc9bb68f381f2ea6c2ed8634f214283187
aallm004 <abigailrosea10@gmail.com>
2024-05-14 22:15:09 +0000

    space

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -27,10 +27,13 @@ def text_indentation(text):
     if not isinstance(text, str):
         raise TypeError("text must be a string")
     new = ""
-    for i in range(len(text)):
-        if text[i] in [".", ":","?"]:
-            new += text[i]
-            new += "\n\n"
-        else:
-            new += text[i]
-    print(new, end="")
+    for i in text:
+        if i == " " and len(new_text) == 0:
+            continue
+        elif i == " " and new_text[-1] in "\n":
+            continue
+        new_text += i
+        if i in ".?:!":
+            new_text += "\n\n"
+
+    print(new_text, end="")
