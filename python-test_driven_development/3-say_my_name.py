dbd885fac40463f8e569fc45c6d06c039a57e555
aallm004 <abigailrosea10@gmail.com>
2024-05-15 00:17:01 +0000

    ehh

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -8,31 +8,4 @@ Functions:
 """
 
 
-def say_my_name(first_name, last_name=""):
-
-    """
-    Function that returns a formatted string with the first name and last name
-
-    Args:
-        first_name (str): first name
-        last_name (str): last name
-
-    Raises:
-        TypeError: If first_name and last_name are not strings
-
-    Return:
-        str: A formatted string with the first name and last name.
-    """
-
-    if not isinstance(first_name, str):
-        """Checking to see if the first_name is a string"""
-        raise TypeError("first_name must be a string")
-    if not isinstance(last_name, str):
-        """Checking to see if the last_name is a string"""
-        raise TypeError("last_name must be a string")
-
-    if last_name:
-        print("My name is {} {}".format(first_name, last_name))
-        """Printing first_name and last_name"""
-    else:
-        print("My name is {} ".format(first_name))
+a
