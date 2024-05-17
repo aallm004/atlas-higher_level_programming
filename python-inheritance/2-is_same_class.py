f9ddd24608e7226d7a968d0f3a9f196ff73205f0
aallm004 <abigailrosea10@gmail.com>
2024-05-17 17:51:03 +0000

    task 2 commit 1

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+"""funciton that returns True if obj is exactly an instance
+    of the specified class; otherwise False"""
+
+
+def is_same_class(obj, a):
+    """
+    funciton that returns True if obj is exactly an instance
+    of the specified class; otherwise False
+    """
+    if type(obj) is a:
+        return True
+    else:
+        return False
