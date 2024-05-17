4c4657d1ecacd3517f2ea53db805e2593aa9b0b5
aallm004 <abigailrosea10@gmail.com>
2024-05-17 18:00:52 +0000

    task 4 commit 1

--- python-inheritance/4-inherit_from.py
+++ python-inheritance/4-inherit_from.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+"""Write a function that returns True if the object is an
+instance of a class that inherited (directly or indirectly)
+from the specified class ; otherwise False. """
+
+
+def inherits_from(obj, a):
+    """returns True if obj is an instance of a subclass
+    of given class"""
+    if isinstance(obj,a) and type(obj) is not a:
+        return True
+    else:
+        return False
