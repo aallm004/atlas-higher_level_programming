3da47a080610c06cef59df97116ce3dd7b393824
aallm004 <abigailrosea10@gmail.com>
2024-05-17 18:53:56 +0000

    task 8 commit 1

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+"""
+write a class Rectange that inherits from BaseGeometry
+"""
+BaseGeometry = __import__("7-base_geometry").BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """
+    class called Rectangle
+    """
+    def __init__(self, width, height):
+        """
+        function __init__
+        """
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
