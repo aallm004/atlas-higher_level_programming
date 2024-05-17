b429c9fe96c5e6182dbb521615306b7f7a10db0e
aallm004 <abigailrosea10@gmail.com>
2024-05-17 20:08:52 +0000

    whitespace

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Write a class Square that inherits from 
+Write a class Square that inherits from
 Rectangle (9-rectangle.py). (task based on 10-square.py).
 """
 Rectangle = __import__('9-rectangle').Rectangle
