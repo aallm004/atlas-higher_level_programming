7f64bfa4f990b117c278a0cfa6d0f5ccf9d1fb75
aallm004 <abigailrosea10@gmail.com>
2024-05-17 18:12:10 +0000

    task 7 commit 1

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+"""New class that is called BaseGeometry"""
+
+
+class BaseGeometry:
+    """class with public instance method to raise exception"""
+    def area(self):
+        raise Exception("area() is not implemented")
+    
+    def integer_validator(self, name, value):
+        if not isinstance(value, int):
+            raise TypeError("<name> must be an integer")
+        elif value <= 0:
+            raise TypeError("<name> must be greater than 0")
