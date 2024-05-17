5e56ff9b5983bb2449c82d58e258a7ab3d10c44b
aallm004 <abigailrosea10@gmail.com>
2024-05-17 19:54:50 +0000

    task ten, does copy work?

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -1,1 +1,24 @@
 #!/usr/bin/python3
+"""
+Class Square that inherits from Rectangle
+"""
+
+
+Rectangle = __import__("9-rectangle.py").Rectangle
+
+class Square(Rectangle):
+
+    def __init__(self, size):
+        """
+        func __init__
+        """
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+    def __str__(self):
+        """
+        fucntion __str__
+        """
+        return "[Square] {}/{}".format(self.__size, self.__size)
+7
