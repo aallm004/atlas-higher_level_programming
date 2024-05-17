b57d70cb9462a422d808e9521d03982c4dab3852
aallm004 <abigailrosea10@gmail.com>
2024-05-17 17:03:04 +0000

    documentation added to task 1

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
+"""New class called MyList that prints a sorted list in ascending order."""
 class MyList(list):
+    """
+    A class "MyList" is initialized
+    """
     def print_sorted(self):
+        """prints the list sorted in ascending order."""
         self.sort()
 
         print(self)
