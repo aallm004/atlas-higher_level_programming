9dd1f16790e771be39ec3550d7bee642bcdd1d2a
aallm004 <abigailrosea10@gmail.com>
2024-05-17 20:02:32 +0000

    task 11 commit 1

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -1,1 +1,22 @@
 #!/usr/bin/python3
+"""
+Write a class Square that inherits from Rectangle (9-rectangle.py). (task based on 10-square.py).
+"""
+
+class Square(__import__('8-rectangle').Rectangle):
+    """
+    class Square
+    """
+    def __init__(self, size)
+        """
+        func __init__
+        """
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+    def __str__(sef):
+        """
+        func __str__
+        """
+        return "[Square] {}/{}".format(self.__size, self.__size)
