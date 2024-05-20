47ca1007bee24f20aab5a2790e0587c3e7daea45
aallm004 <abigailrosea10@gmail.com>
2024-05-20 19:23:27 +0000

    task 2 commit 1

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+"""
+Write a function that appends a string
+at the end of a text file (UTF8) and
+returns the number of characters added
+"""
+
+
+def append_write(filename="", text=""):
+    """appends a string at end of text file
+    and returns # of char added"""
+    with open(filename, 'a') as f:
+        return f.write(text)
