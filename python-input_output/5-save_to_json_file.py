db36c44410e9aecc846d365939d1250347287eda
aallm004 <abigailrosea10@gmail.com>
2024-05-20 20:08:18 +0000

    task 5 commit 1

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+"""
+function that writes an Object to a text file,
+using a JSON representation
+"""
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """writes an obj to text file using JSON"""
+    with open ('my_obj', 'w') as f:
+        return json.dump(my_obj, f)
