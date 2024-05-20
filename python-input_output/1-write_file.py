2addd6918113a109cb0c312f5812dfdcaff5a0cc
aallm004 <abigailrosea10@gmail.com>
2024-05-20 19:14:23 +0000

    task 1 commit 1

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+"""
+Write a function that writes a string to a text file (UTF8) and 
+returns the number of characters written
+
+"""
+
+
+def write_file(filename="", text=""):
+    """writes string to a text file and returns number of chars"""
+    with open(filename, 'w') as f:
+        return len(text)
