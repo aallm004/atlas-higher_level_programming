2774a9fa85735762fd243427affca8d056fe0dfc
aallm004 <abigailrosea10@gmail.com>
2024-05-20 19:02:15 +0000

    task 0 commit 3

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
+"""
+Write a function that reads a text file (UTF8) 
+and prints it to stdout
+"""
 def read_file(isfile):
-    "reads text file and prints to stdout"
+    """reads text file and prints to stdout"""
     with open(isfile) as f:
         for line in f:
             print(line, end="")
