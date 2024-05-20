83bc171304e066e974eda45e2f648fbe3cdde584
aallm004 <abigailrosea10@gmail.com>
2024-05-20 19:41:53 +0000

    pep8

--- python-input_output/3-to_json_string.py
+++ python-input_output/3-to_json_string.py
@@ -2,6 +2,8 @@
 """
 function that returns the JSON representation of an object (string)
 """
+
+
 def to_json_string(my_obj):
     """returns JSON representation of an object(string)"""
     import json
