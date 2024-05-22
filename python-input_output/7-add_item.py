174aa0411abac5153ed1f69a62cd04dfe6b325db
aallm004 <abigailrosea10@gmail.com>
2024-05-22 02:12:15 +0000

    extra s

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -9,7 +9,7 @@ import sys
 save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
 load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 try:
-    i = load_from_json_file("add_items.json")
+    i = load_from_json_file("add_item.json")
 except:
     i = []
 for x in range(1, len(sys.argv)):
