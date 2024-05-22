95a9ed0ff8ba25c621e19774af798d23cc865469
aallm004 <abigailrosea10@gmail.com>
2024-05-22 02:55:12 +0000

    push it

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+""" numero diez """
+
+
+class Student:
+    """Student class
+    """
+    def __init__(self, first_name, last_name, age):
+        """__init__ method"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if attrs is None:
+            return self.__dict__
+        else:
+            return self
