a4317f341c070f13d8857fcb2549599f21d7aa02
aallm004 <abigailrosea10@gmail.com>
2024-05-22 03:23:34 +0000

    task 12 12 12

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -1,1 +1,19 @@
 #!/usr/bin/python3
+"""function that returns a list of
+integers representing the Pascal's triangle of n:"""
+
+
+def pascal_triangle(n):
+    """technical interview prep"""
+    if n <= 0:
+        return []
+
+    pt = [[1]]
+    while len(pt) != n:
+        triangle = pt[-1]
+        hold = [1]
+        for o in range(len(triangle) - 1):
+            hold.append(triangle[o] + triangle[o + 1])
+        hold.append(1)
+        pt.append(hold)
+    return pt
