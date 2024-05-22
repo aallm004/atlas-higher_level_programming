f3529305590edd04578583ade708bb2d0bab8550
aallm004 <abigailrosea10@gmail.com>
2024-05-22 02:42:35 +0000

    task 9 commit 1

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+""" numero nueve """
+
+
+class Student:
+    """Student class
+    """
+    def __init__(self, first_name, last_name, age):
+        """__init__ method"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+
+        return self
