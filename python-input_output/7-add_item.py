2558168f85cb411241907a851816f19c1db63a57
aallm004 <abigailrosea10@gmail.com>
2024-05-22 02:05:03 +0000

    task 7 commit 932

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -3,3 +3,16 @@
 script that adds all arguments to a Python list,
 and then save them to a file
 """
+
+
+import sys
+
+save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
+load_from_json_file =__import__("6-load_from_json_file").load_from_json_file
+try:
+    i = load_from_json_file("add_items.json")
+except:
+    i = []
+for x in range(1, len(sys.argv)):
+    i.append(sys.argv[x-1])
+save_to_json_file(i, "add_item.json")
