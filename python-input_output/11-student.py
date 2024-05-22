6a881d6d958443d27e6a766b59d521fd5f578125
aallm004 <abigailrosea10@gmail.com>
2024-05-22 03:13:04 +0000

    task 11 commit 1

--- python-input_output/11-student.py
+++ python-input_output/11-student.py
@@ -1,1 +1,25 @@
 #!/usr/bin/python3
+""" numero diez """
+
+
+class Student:
+    """Student class
+    """
+    def __init__(self, first_name, last_name, age):
+        """__init__ method"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if attrs is None:
+            return self.__dict__
+        else:
+            new_space = {}
+            for k, v in self.__dict__.items():
+                if k in attrs:
+                    new_space[k] = v
+            return new_space
+
+    def reload_from_json(self, json):
+        for k, v in json.items()
