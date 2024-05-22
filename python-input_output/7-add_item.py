a32346c0970149b26678e4c0df8aca93aff2496c
aallm004 <abigailrosea10@gmail.com>
2024-05-22 02:22:53 +0000

    added error message

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -10,7 +10,8 @@ save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
 load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 try:
     i = load_from_json_file("add_item.json")
-except:
+except FileNotFoundError:
+    print("The file 'add_item.json' was not found. Creating a new list.")
     i = []
 for x in range(1, len(sys.argv)):
     i.append(sys.argv[x])
