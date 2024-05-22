a54c01049501fe861384c025e1baad1085218649
aallm004 <abigailrosea10@gmail.com>
2024-05-22 02:15:44 +0000

    else?

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -8,9 +8,9 @@ import sys
 
 save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
 load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
-try:
+if:
     i = load_from_json_file("add_item.json")
-except:
+else:
     i = []
 for x in range(1, len(sys.argv)):
     i.append(sys.argv[x])
