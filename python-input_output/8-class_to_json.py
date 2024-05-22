ebf3e92624f99faac2234b4617014e9158a75f84
aallm004 <abigailrosea10@gmail.com>
2024-05-22 02:31:59 +0000

    task 8 commit 1

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -1,1 +1,20 @@
 #!/usr/bin/python3
+"""
+function that returns the dictionary description
+with simple data structure (list, dict, string, int, boolean)
+for JSON serialization of an object
+"""
+
+def class_to_json(obj):
+    """
+    function that returns the dictionary description
+    with simple data structure (list, dict, string, int, boolean)
+    for JSON serialization of an object
+    
+    Args:
+        obj: Object to be serialized
+
+    Returns:
+        dict: dictionary representation of the object
+    """
+    return obj.__dict__
