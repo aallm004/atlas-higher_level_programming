aacead5aec831d24b3e352784bbf925daf6808c6
aallm004 <abigailrosea10@gmail.com>
2024-05-25 20:56:36 +0000

    documentation

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
+"""Base Class"""
 
 class Base:
+    """Base Class is created"""
 
     id = 0
     __nb_objects = 0
 
     def __init__(self, id=None):
+        """incrementation of nb"""
         if id is not None:
             self.id = id
         else:
