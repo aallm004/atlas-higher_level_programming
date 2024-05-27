c10ca2f8bf964a0ff5a4c8b283cb0bd9f5613e44
aallm004 <abigailrosea10@gmail.com>
2024-05-27 20:07:14 +0000

    doc

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -18,6 +18,7 @@ class Base:
             self.id = Base.__nb_objects
 
     def to_json_string(list_dictionaries):
+        """json comes to town"""
         if list_dictionaries is None:
             return []
         else:
