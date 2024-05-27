480e3933fe7c56923e5cc6c1a1b1fde610a41a10
aallm004 <abigailrosea10@gmail.com>
2024-05-27 20:28:01 +0000

    staticmethod added

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -25,6 +25,7 @@ class Base:
         else:
             return json.dumps(list_dictionaries)
 
+    @staticmethod
     def save_to_file(cls, list_objs):
         """save to file"""
         with open(self, "w", encoding="utf-8") as f:
