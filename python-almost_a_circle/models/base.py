da7b97e9c8c7a51cc041332c08504af266628570
aallm004 <abigailrosea10@gmail.com>
2024-05-27 23:24:03 +0000

    forgotten :

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -50,7 +50,7 @@ class Base:
 
         if cls.__name__== 'Rectangle':
             obj = cls(1, 1)
-        elif cls.__name__ == 'Square'
+        elif cls.__name__ == 'Square':
             obj = cls(1)
         
         obj.update(**dictionary)
