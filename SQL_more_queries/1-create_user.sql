3b2099b4aa2f9c7ba5289da262d7818ea7846da6
aallm004 <abigailrosea10@gmail.com>
2024-06-17 16:06:19 -0500

    corrected another spelling error in task one

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,3 +1,3 @@
 -- creates server user user_0d_1
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVELAGES ON *.* TO 'user_0d_1'@'localhost';
+GRANT ALL PRIVELEGES ON *.* TO 'user_0d_1'@'localhost';
