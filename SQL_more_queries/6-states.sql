ec75a173487b676790ebf1855efbcfd15cc05fa8
aallm004 <abigailrosea10@gmail.com>
2024-06-17 16:10:10 -0500

    missing quotes

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -4,5 +4,5 @@ USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS 'states' (
     'id' INT UNIQUE NOT NULL AUTO_INCREMENT,
     'name' VARCHAR(256) NOT NULL,
-    PRIMARY KEY (id)
+    PRIMARY KEY ('id')
 );
