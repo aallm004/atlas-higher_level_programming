39b605e78a1c8fe8179d7f30b24c95e20e9ff539
aallm004 <abigailrosea10@gmail.com>
2024-06-17 17:07:33 -0500

    missing space for documentation

--- SQL_more_queries/14-my_genres.sql
+++ SQL_more_queries/14-my_genres.sql
@@ -1,4 +1,4 @@
---uses hbtn database to list all genres of the show DEXTER
+-- uses hbtn database to list all genres of the show DEXTER
 SELECT tv_genres.name FROM tv_shows
 JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
