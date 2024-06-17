fa8fa54abd3bc417f14fd97837a9578c532e254e
aallm004 <abigailrosea10@gmail.com>
2024-06-17 16:54:03 -0500

    corrected misspelling on task 12

--- SQL_more_queries/12-no_genre.sql
+++ SQL_more_queries/12-no_genre.sql
@@ -1,6 +1,6 @@
 -- lists all shows contained in htbn without a genre linked
-SELECT tv_show.title, tv_show_genres.genre_id 
+SELECT tv_shows.title, tv_show_genres.genre_id 
 FROM tv_shows
 LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 WHERE tv_show_genres.genre_id IS NULL
-ORDER BY tv_shows.title ASC, tv_show_genre_id ASC;
+ORDER BY tv_shows.title, tv_show_genres, genre_id;
