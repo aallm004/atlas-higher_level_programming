c071a08d10ef2e4af9dc3f1b3840e268ae28b475
aallm004 <abigailrosea10@gmail.com>
2024-06-17 17:07:09 -0500

    fourteen

--- SQL_more_queries/14-my_genres.sql
+++ SQL_more_queries/14-my_genres.sql
@@ -1,6 +1,6 @@
 --uses hbtn database to list all genres of the show DEXTER
-SELECT tv_genres.name FROM tv_genres
-INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
-INNER JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
+SELECT tv_genres.name FROM tv_shows
+JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
 WHERE tv_shows.title = 'Dexter'
-ORDER BY tv_genres.name;
+ORDER BY tv_genres.name ASC;
