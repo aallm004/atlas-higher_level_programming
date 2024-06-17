7299da3319bbd24bf5c37bcc39c52e9d64c4b3c5
aallm004 <abigailrosea10@gmail.com>
2024-06-17 16:38:06 -0500

    pushy

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -1,9 +1,9 @@
 -- create database and table
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 USE hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS 'cities' (
-    'id' INT AUTO_INCREMENT NOT NULL, 
-    'state_id' INT NOT NULL,
-    'name' VARCHAR(256) NOT NULL
+CREATE TABLE IF NOT EXISTS cities (
+    id INT AUTO_INCREMENT NOT NULL, 
+    state_id INT NOT NULL,
+    name VARCHAR(256) NOT NULL
     PRIMARY KEY(id)
 );
