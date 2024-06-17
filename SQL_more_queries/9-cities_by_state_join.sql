be57bf8de37094c79af9de04bd25471976ec4a72
aallm004 <abigailrosea10@gmail.com>
2024-06-17 16:42:18 -0500

    corrected states to state

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,4 +1,4 @@
 -- script that lists all cities containeed in database
 SELECT cities.id, cities.name, states.name FROM cities
-LEFT JOIN states ON state.id = cities.states_id
+LEFT JOIN states ON state.id = cities.state_id
 ORDER BY cities.id;
