767bae235663cd750698d1fe8dab06207f78f7f1
aallm004 <abigailrosea10@gmail.com>
2024-06-17 16:37:00 -0500

    space after key between id

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -5,5 +5,5 @@ CREATE TABLE IF NOT EXISTS 'cities' (
     'id' INT AUTO_INCREMENT NOT NULL, 
     'state_id' INT NOT NULL,
     'name' VARCHAR(256) NOT NULL
-    PRIMARY KEY (id)
+    PRIMARY KEY(id)
 );
