12b6fd9eff083d6d7d1c866f0b9962b1bf84d8c5
aallm004 <abigailrosea10@gmail.com>
2024-06-17 16:19:34 -0500

    trying

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,4 +1,4 @@
 --script that lists all cities containeed in database
-SELECT cities.id, cities.name, states.name FROM cities
-LEFT JOIN states ON cities.state_id = states.id
-ORDER BY id ASC;
+SELECT 'cities'.'id', 'cities'.''name', 'states'.'name' FROM 'cities'
+LEFT JOIN 'states' ON 'cities'.'state_id' = 'states'.'id'
+ORDER BY 'cities'.'id';
