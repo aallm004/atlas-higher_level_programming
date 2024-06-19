0b6a02f5e41679359cf58d69e75695fbe058b943
aallm004 <abigailrosea10@gmail.com>
2024-06-19 08:09:43 -0500

    task 3

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,1 +1,21 @@
 #!/usr/bin/python3
+"""Script that takes an arg and disp values in the states table"""
+
+import sys
+import MySQLdb
+
+
+if __name__ == "__main__":
+
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3])
+
+    cur = db.cursor()
+
+    cur.execute("""SELECT * FROM states WHERE
+                BINARY name = '{}' ORDER BY id"""
+                .format(sys.argv[4].replace("'", "")))
+    results = cur.fetchall()
+
+    for state in results:
+        print(state)
