2c1bded8deba95e69a9c4e82c53bd394216c3351
aallm004 <abigailrosea10@gmail.com>
2024-06-19 15:19:32 -0500

    do quotation styles matter?

--- python-object_relational_mapping/model_city.py
+++ python-object_relational_mapping/model_city.py
@@ -9,9 +9,9 @@ Base = declarative_base()
 
 class City(Base):
     """Start of City class"""
-    __tablename__ = 'cities'
+    __tablename__ = "cities"
     id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
     name = Column(String(128), nullable=False)
-    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
+    state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
 
     state = relationship("State", back_populates="cities")
