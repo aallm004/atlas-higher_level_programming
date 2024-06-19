c7194d4c68f1078b5d984ecd81c7a7f32d4a64f0
aallm004 <abigailrosea10@gmail.com>
2024-06-18 20:08:59 -0500

    moving things around

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 """Script that lists all states from the database hbtn_0e_0_usa"""
 
-if __name__ == "__main__":
-    import sys
-    import MySQLdb
+import sys
+import MySQLdb
 
+if __name__ == "__main__":
+    
     username = sys.argv[1]
     password = sys.argv[2]
     database_name = sys.argv[3]
