c0706d1fe7c29826f5ac28585fb41ccb9047897b
aallm004 <abigailrosea10@gmail.com>
2024-06-19 07:43:51 -0500

    indentation fix

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -8,8 +8,8 @@ import MySQLdb
 
 if __name__ == "__main__":
 
-    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
-                            passwd=sys.argv[2], db=sys.argv[3])
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1], 
+                         passwd=sys.argv[2], db=sys.argv[3])
 
     cur = db.cursor()
 
