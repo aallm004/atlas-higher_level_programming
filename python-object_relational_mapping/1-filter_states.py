ee4ac8b0b849824d3a937abc0f026e253ee7bb17
aallm004 <abigailrosea10@gmail.com>
2024-06-19 07:59:06 -0500

    pycode

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -12,7 +12,8 @@ if __name__ == "__main__":
 
     cur = db.cursor()
 
-    cur.execute("""SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC;""")
+    cur.execute("""SELECT * FROM states WHERE name
+                LIKE 'N%' ORDER BY id ASC;""")
 
     results = cur.fetchall()
 
