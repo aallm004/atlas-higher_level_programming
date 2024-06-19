5acfdba53b69e993235ab84bf25090c92873b971
aallm004 <abigailrosea10@gmail.com>
2024-06-19 10:47:02 -0500

    sqlalchemy

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,13 +1,20 @@
 #!/usr/bin/python3
-"""defines function that creates base model"""
+"""Class def of State and instance"""
+import sys
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
 
-from sqlalchemy import Column, Integer, String
-from sqlalchemy.ext.declarative import declarative_base
 
-Base = declarative_base()
+if __name__ == "__main__":
+    eng = create_engine('mysql+mysqldb://{}:{}@localhose/{}'
+                        .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                        pool_pre_ping=True)
+    
+    Base.metadata.create_all(eng)
+    Session = sessionmaker(bind=eng)
+    session = Session()
+    for item in session.query(State).order_by(State.id):
+        print("{}: {}".format(item.id, item.name))
 
-class State(Base):
-    """Start of State Class"""
-    __tablename__ = 'states'
-    name = Column(String(128), nullable=False)
-    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+    session.close()
