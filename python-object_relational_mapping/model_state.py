1ca83c1c7f9e5aa89eedb8bbdd48bd264a19058e
aallm004 <abigailrosea10@gmail.com>
2024-06-19 10:47:32 -0500

    documentation

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -7,6 +7,7 @@ from sqlalchemy.orm import sessionmaker
 
 
 if __name__ == "__main__":
+    """comment"""
     eng = create_engine('mysql+mysqldb://{}:{}@localhose/{}'
                         .format(sys.argv[1], sys.argv[2], sys.argv[3]),
                         pool_pre_ping=True)
