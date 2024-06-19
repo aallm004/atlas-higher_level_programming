fb972390cab8ab95c7dd28dfc485a96597c56a1c
aallm004 <abigailrosea10@gmail.com>
2024-06-19 08:47:09 -0500

    task 7

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,15 +1,21 @@
 #!/usr/bin/python3
 """Class def of State and instance"""
-from sqlalchemy import Column, Integer, String
-from sqlalchemy.ext.declarative import declarative_base
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
 
-Base = declarative_base()
+if __name__ == "__main__":
+    eng = create_engine('mysql+mysqldb://{}:{}@localhose/{}'
+                        .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                        pool_pre_ping=True)
+    
+    Base.metadata.create_all(eng)
+    Session = sessionmaker(bind=eng)
+    session = Session()
+    for item in session.query(State).order_by(State.id):
+        print("{}: {}".format(item.id, item.name))
 
-
-class State(Base):
-    """ Class State that inherits from Base """
-    __tablename__ = 'states'
-    id = Column(Integer, primary_key=True, nullable=False,
-                autoincrement="auto", unique=True)
-    name = Column(String(128), nullable=False)
+    session.close()
+    
