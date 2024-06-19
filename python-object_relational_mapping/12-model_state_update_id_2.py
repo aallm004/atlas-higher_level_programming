fc307589267adeed941c89bae146079804a796cc
aallm004 <abigailrosea10@gmail.com>
2024-06-19 14:26:05 -0500

    task 12

--- python-object_relational_mapping/12-model_state_update_id_2.py
+++ python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,1 +1,28 @@
 #!/usr/bin/python3
+"""Lists all states from database"""
+
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        print("Usage: {} <mysql_username> <mysql_password> <db_name>".format(sys.argv[0]))
+        sys.exit(1)
+
+    username, password, db_name = sys.argv[1], sys.argv[2], sys.argv[3]
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(username, password, db_name))
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    state_to_update = session.query(State).filter_by(id=2).first()
+    if state_to_update:
+        state_to_update.name = "New Mexico"
+        session.commit()
+        print("State name updated successfully!")
+    else:
+        print("State with id 2 not found.")
+
+    session.close()
