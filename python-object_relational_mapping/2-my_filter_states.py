43e3bb15a4c750c9aff3358c3f33c4e124be2139
aallm004 <abigailrosea10@gmail.com>
2024-06-19 08:06:38 -0500

    sys.

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -14,7 +14,7 @@ if __name__ == "__main__":
 
     cur.execute("""SELECT * FROM states WHERE
                 BINARY name = '{}' ORDER BY id"""
-                .format(argv[4]))
+                .format(sys.argv[4]))
 
     results = cur.fetchall()
 
