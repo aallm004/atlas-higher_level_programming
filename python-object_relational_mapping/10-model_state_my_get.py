5d59fe25818b9b5569d637f4b5603ba41db67163
aallm004 <abigailrosea10@gmail.com>
2024-06-19 12:31:06 -0500

    task 10

--- python-object_relational_mapping/10-model_state_my_get.py
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -1,1 +1,28 @@
 #!/usr/bin/python3
+"""Lists all states from database"""
+
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    if len(sys.argv) != 5:
+        print("Usage: {} <mysql_username> <mysql_password> <db_name> <state_name>".format(sys.argv[0]))
+        sys.exit(1)
+
+    username, password, db_name, state_name = sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4]
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(username, password, db_name),
+                           pool_pre_ping=True)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    state = session.query(State).filter(State.name == state_name).first()
+
+    if state:
+        print(state.id)
+    else:
+        print("Not found")
+
+    session.close()
