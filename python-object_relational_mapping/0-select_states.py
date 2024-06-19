141f6b6da6171fbf294dc6cae0076c2870c0e4c3
aallm004 <abigailrosea10@gmail.com>
2024-06-18 20:20:28 -0500

    eh

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -2,18 +2,18 @@
 """Script that lists all states from the database hbtn_0e_0_usa"""
 
 
-if __name__ == "__main__":
-    import sys
-    import MySQLdb
+import sys
+import MySQLdb
 
+def liststates():
     username = sys.argv[1]
     password = sys.argv[2]
     database_name = sys.argv[3]
 
-    db = MySQLdb.connect(host="localhost", user=username,
+    connection = MySQLdb.connect(host="localhost", user=username,
                          port=3306, pw=password, db_name=database_name)
 
-    cur = db.cursor()
+    cur = connection.cursor()
 
     cur.execute("SELECT * FROM states ORDER BY states.id ASC")
 
@@ -22,5 +22,8 @@ if __name__ == "__main__":
     for row in rows:
         print(row)
 
-        cur.close()
-        db.close()
+    cur.close()
+    connection.close()
+
+if __name__ == "__main__":
+    liststates()
