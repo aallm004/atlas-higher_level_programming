b32d66d59e6a39be871550c0350efe470714126c
aallm004 <abigailrosea10@gmail.com>
2024-06-19 08:17:47 -0500

    task 5

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -1,1 +1,21 @@
 #!/usr/bin/python3
+#!/usr/bin/python3
+"""Script that takes name of state as arg and lists all cities"""
+import sys
+import MySQLdb
+
+
+if __name__ == "__main__":
+
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3])
+
+    cur = db.cursor()
+
+    cur.execute(""""SELECT cities.name FROM cities INNER JOIN states ON \
+                cities.state_id = states.id WHERE states.name = %s \
+                ORDER BY cities.id ASC", (argv[4],)""")
+    results = cur.fetchall()
+    print(", ".join([row[0] for row in query_rows]))
+    cur.close()
+    db.close()
