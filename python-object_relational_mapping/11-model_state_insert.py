6b99df39aabfeaea5272eb04811eb4cf7426fd9b
aallm004 <abigailrosea10@gmail.com>
2024-06-19 12:34:13 -0500

    pycode

--- python-object_relational_mapping/11-model_state_insert.py
+++ python-object_relational_mapping/11-model_state_insert.py
@@ -1,1 +1,27 @@
 #!/usr/bin/python3
+"""Lists all states from database"""
+
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        print("Usage: {} <mysql_username> <mysql_password> <db_name>".format(sys.argv[0]))
+        sys.exit(1)
+
+    username, password, db_name = sys.argv[1], sys.argv[2], sys.argv[3]
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(username, password, db_name),
+                           pool_pre_ping=True)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    new_state = State(name="Louisiana")
+    session.add(new_state)
+    session.commit()
+
+    print(new_state.id)
+
+    session.close()
