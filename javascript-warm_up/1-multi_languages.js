a31c55c34ec61b53a250d5da776af62cadc63545
aallm004 <abigailrosea10@gmail.com>
2024-07-18 11:05:02 -0500

    constant not letting

--- javascript-warm_up/1-multi_languages.js
+++ javascript-warm_up/1-multi_languages.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
-let a = 'C is fun'
-let b = 'Python is cool'
-let c = 'JavaScript is amazing'
-console.log(a)
-console.log(b)
-console.log(c)
+const a = 'C is fun';
+const b = 'Python is cool';
+const c = 'JavaScript is amazing';
+console.log(a);
+console.log(b);
+console.log(c);
