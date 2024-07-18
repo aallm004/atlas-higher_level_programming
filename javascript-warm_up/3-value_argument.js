3ea41cc7ebd18d2b682cabbe8dc69e6b42ae0304
aallm004 <abigailrosea10@gmail.com>
2024-07-18 16:32:33 -0500

    task 3

--- javascript-warm_up/3-value_argument.js
+++ javascript-warm_up/3-value_argument.js
@@ -1,8 +1,13 @@
 #!/usr/bin/node
 const arg = process.argv.slice(2);
+let count = 0;
 
-if (arg === 0) {
+for (const item of arg) {
+  count++;
+}
+
+if (count === 0) {
   console.log('No argument');
 } else {
-  console.log(arg);
+    console.log(arg);
 }
