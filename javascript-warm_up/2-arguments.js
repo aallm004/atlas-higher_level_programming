46dff83568c0d9acf132bbaf4e1db5d2f734563d
aallm004 <abigailrosea10@gmail.com>
2024-07-18 16:06:01 -0500

    forgot to change name of initialized variable from firstArg to arg

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,9 +1,10 @@
 #!/usr/bin/node
 
 const arg = process.argv.slice(2);
-if (firstArg.length === 0) {
+
+if (arg.length === 0) {
   console.log('No argument');
-} else if (firstArg.length === 1) {
+} else if (arg.length === 1) {
     console.log("Argument found");
 } else {
   console.log("Arguments found");
