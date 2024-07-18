25b7aeacf7f6a1476f14211a9cd472ed50b090e4
aallm004 <abigailrosea10@gmail.com>
2024-07-18 16:33:55 -0500

    simplifyyy

--- javascript-warm_up/3-value_argument.js
+++ javascript-warm_up/3-value_argument.js
@@ -1,13 +1,8 @@
 #!/usr/bin/node
 const arg = process.argv.slice(2);
-let count = 0;
 
-for (const item of arg) {
-  count++;
-}
-
-if (count === 0) {
+if (arg.length === 0) {
   console.log('No argument');
 } else {
-    console.log(arg);
+    console.log(arg[0]);
 }
