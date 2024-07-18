c55773cbb39437d8738bd520fc261938aa96d740
aallm004 <abigailrosea10@gmail.com>
2024-07-18 16:01:13 -0500

    eh?

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -3,8 +3,10 @@
 const arg = 0;
 const firstArg = arg[0];
 
-if (firstArg === undefined) {
+if (firstArg === 0) {
   console.log('No argument');
-} else if ({
-  console.log("No argument");
+} else if (firstArg === 1) {
+    console.log("Argument found");
+} else {
+  console.log("Arguments found");
 }
