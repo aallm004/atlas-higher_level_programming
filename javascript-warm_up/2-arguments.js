078f2243250f0c611deab2b83460fa13c0d82f03
aallm004 <abigailrosea10@gmail.com>
2024-07-18 16:06:53 -0500

    single quotes not double

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -5,7 +5,7 @@ const arg = process.argv.slice(2);
 if (arg.length === 0) {
   console.log('No argument');
 } else if (arg.length === 1) {
-    console.log("Argument found");
+  console.log('Argument found');
 } else {
-  console.log("Arguments found");
+  console.log('Arguments found');
 }
