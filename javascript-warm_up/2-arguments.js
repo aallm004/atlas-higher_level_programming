c0b4c0cd0506bca513e96a1c68b4aee8ef366717
aallm004 <abigailrosea10@gmail.com>
2024-07-18 16:28:36 -0500

    task 3

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,5 +1,4 @@
 #!/usr/bin/node
-
 const arg = process.argv.slice(2);
 
 if (arg.length === 0) {
--- javascript-warm_up/3-value_argument.js
+++ javascript-warm_up/3-value_argument.js
@@ -1,1 +1,8 @@
 #!/usr/bin/node
+const arg = process.argv.slice(2);
+
+if (arg.length === 0) {
+  console.log('No argument');
+} else {
+  console.log(arg);
+}
