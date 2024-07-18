b979afe10c1cf2d8810fddec75e7575abc591ec0
aallm004 <abigailrosea10@gmail.com>
2024-07-18 16:39:39 -0500

    task 4

--- javascript-warm_up/4-concat.js
+++ javascript-warm_up/4-concat.js
@@ -1,1 +1,8 @@
 #!/usr/bin/node
+const args = process.argv.slice(2);
+
+if (args.length < 2) {
+    console.log('Please provide two arguments');
+} else {
+    console.log(`${args[0]} is ${args[1]}`);
+}
