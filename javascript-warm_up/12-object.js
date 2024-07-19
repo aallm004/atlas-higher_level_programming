8a33c4e9ca120f2fac7f516210b4470098d0b6a2
aallm004 <abigailrosea10@gmail.com>
2024-07-19 10:24:49 -0500

    task 12

--- javascript-warm_up/12-object.js
+++ javascript-warm_up/12-object.js
@@ -1,1 +1,10 @@
 #!/usr/bin/node
+const x = process.argv[2];
+const myObject = {
+    type: 'object',
+    value: 12
+  };
+  console.log(myObject);
+    typeof (myObject);
+    myObject.value = 89;
+  console.log(myObject);
