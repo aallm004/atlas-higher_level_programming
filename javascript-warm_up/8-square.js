28546acc318eceb92820340214122f68bf757e71
aallm004 <abigailrosea10@gmail.com>
2024-07-19 09:16:19 -0500

    semistandard

--- javascript-warm_up/8-square.js
+++ javascript-warm_up/8-square.js
@@ -2,13 +2,13 @@
 const x = process.argv[2];
 
 if (isNaN(x)) {
-    console.log("Missing size")
+  console.log('Missing size');
 } else {
-    for (let i = 0; i < x; i++) {
-        let row = "";
-        for (let j = 0; j < x; j++) {
-            row += "X";
-        }
-        console.log(row);
+  for (let i = 0; i < x; i++) {
+    let row = '';
+    for (let j = 0; j < x; j++) {
+      row += 'X';
     }
+    console.log(row);
+  }
 }
