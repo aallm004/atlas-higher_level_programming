e9124b1200d2b05161f45bcc0ceed24d5a32ca28
aallm004 <abigailrosea10@gmail.com>
2024-07-21 15:17:25 -0500

    semistandard correction

--- javascript_objects_scopes_closures/3-rectangle.js
+++ javascript_objects_scopes_closures/3-rectangle.js
@@ -2,14 +2,15 @@
 class Rectangle {
   constructor (w, h) {
     if (w <= 0 && h <= 0) {
-        return {};
+      return {};
     }
-        this.width = w;
-        this.height = h;
-    }
-    print() {
-        for (let i = 0; i < this.height; i++) {
-            console.log('X'.repeat(this.width));
+    this.width = w;
+    this.height = h;
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      console.log('X'.repeat(this.width));
     }
   }
 }
