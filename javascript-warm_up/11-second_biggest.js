22229cabc4c44adfa3e8ad4d09a0da39aa3d3bd7
aallm004 <abigailrosea10@gmail.com>
2024-07-22 22:59:42 -0500

    we don't need no stinkin map

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -3,7 +3,6 @@ const args = process.argv.slice(2);
 if (args.length < 2) {
   console.log(0);
 } else {
-//  const numbers = args.map(Number);
   args.sort((a, b) => b - a);
   console.log(args[1]);
 }
