ddefd24b91c8805e8fad9d4dd6144ce33f35b78d
aallm004 <abigailrosea10@gmail.com>
2024-07-22 22:01:41 -0500

    minor little change

--- javascript_objects_scopes_closures/4-rectangle.js
+++ javascript_objects_scopes_closures/4-rectangle.js
@@ -1,12 +1,11 @@
 #!/usr/bin/node
 class Rectangle {
-  constructor (w, h) {
-    if (w <= 0 && h <= 0) {
-      return {};
+    constructor (w, h) {
+      if (w >= 0 && h >= 0) {
+        this.width = w;
+        this.height = h;
+      }
     }
-    this.width = w;
-    this.height = h;
-  }
 
   print () {
     for (let i = 0; i < this.height; i++) {
