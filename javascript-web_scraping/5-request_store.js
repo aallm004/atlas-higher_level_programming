273fb0514553452299562de0e194964742629392
aallm004 <abigailrosea10@gmail.com>
2024-07-25 15:35:57 -0500

    task 5

--- javascript-web_scraping/5-request_store.js
+++ javascript-web_scraping/5-request_store.js
@@ -1,1 +1,14 @@
 #!/usr/bin/node
+const url = process.argv[2];
+const path = process.argv[3];
+const fs = require('fs');
+const request = require('request');
+
+if ((url !== undefined) && (path !== undefined)) {
+  request(url, (error, response, body) => {
+    if (error) console.log(error);
+    fs.writeFile(path, body, (err) => {
+      if (err) console.log(err);
+    });
+  });
+}
