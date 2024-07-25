4618f103abca4137369f1be3e424c562df75667a
aallm004 <abigailrosea10@gmail.com>
2024-07-25 13:14:29 -0500

    took out unnecessary console.log

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -3,15 +3,14 @@
 const url = process.argv[2];
 const request = require('request');
 
-  if (url !== undefined) {
-request(url, (error, response, body) => {
-  if (error) console.log(error);
+if (url !== undefined) {
+  request(url, (error, response, body) => {
+    if (error) console.log(error);
 
-  const data = JSON.parse(body).results;
+    const data = JSON.parse(body).results;
 
-  let movie = data.filter(x => x.characters.includes(
-    'https://swapi-api.hbtn.io/api/people/18/'))
-    console.log(movie)
-  console.log(movie.length);
-})
-  }
+    const movie = data.filter(x => x.characters.includes(
+      'https://swapi-api.hbtn.io/api/people/18/'));
+    console.log(movie.length);
+  });
+}
