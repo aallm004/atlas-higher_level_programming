25f55378feb0d439ac7228d5adc5507242c09d23
aallm004 <abigailrosea10@gmail.com>
2024-07-25 10:48:57 -0500

    semistandard

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -2,7 +2,7 @@
 const x = process.argv[2];
 const fs = require('fs');
 
-if (x != undefined) {
+if (x !== undefined) {
   fs.readFile(x, (err, data) => {
     if (err) throw err;
     console.log(data.toString());
