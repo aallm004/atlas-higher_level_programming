d63761d59e247ee4dc46b09ddf807f14696fdd59
aallm004 <abigailrosea10@gmail.com>
2024-07-25 12:01:00 -0500

    task 1

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -3,7 +3,7 @@ const x = process.argv[2];
 const data = process.argv[3];
 const fs = require('fs');
 
-if ((x !== undefined) && (data !== undefined)) {
+if ((x !== undefined) || (data !== undefined)) {
 fs.writeFile(x, data, (err) => {
     if (err)
         console.log(err);
