3e118c905277b575b07acf8959edc15cd05fb52b
aallm004 <abigailrosea10@gmail.com>
2024-07-25 12:40:52 -0500

    task 3

--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -1,1 +1,14 @@
 #!/usr/bin/node
+
+const id = process.argv[2];
+let url = 'https://swapi-api.hbtn.io/api/films/' + id
+const request = require('request');
+
+if (id !== undefined) {
+  request(url, (error, response, body) => {
+    if (error) console.log(error);
+
+    let data = JSON.parse(body);
+    console.log(data.title);
+  });
+}
