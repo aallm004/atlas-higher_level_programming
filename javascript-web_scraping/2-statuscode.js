0bf8eaac5077bdda10392390a281b94889c993bc
aallm004 <abigailrosea10@gmail.com>
2024-07-25 12:26:15 -0500

    task 2

--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -1,1 +1,12 @@
 #!/usr/bin/node
+
+const url = process.argv[2];
+const request = require('request');
+
+if (url !== undefined) {
+request(url, (error, response, body) => {
+  if (error) console.log(error);
+
+  console.log('code: ' + response.statusCode);
+})
+}
