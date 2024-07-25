d5714b5beae0bdbda7fb8d0c23d115286dd4327d
aallm004 <abigailrosea10@gmail.com>
2024-07-25 16:04:34 -0500

    task 6

--- javascript-web_scraping/6-completed_tasks.js
+++ javascript-web_scraping/6-completed_tasks.js
@@ -1,1 +1,19 @@
 #!/usr/bin/node
+const url = process.argv[2];
+const request = require('request');
+let out = {}
+
+if (url !== undefined) {
+  request(url, (error, response, body) => {
+    if (error) console.log(error);
+const data = JSON.parse(body);
+    results = data.filter(x => x.completed)
+    results.forEach(element => {
+      if (out[element.userId])
+        out[element.userId] += 1
+      else
+        out[element.userId] = 1
+    })
+    console.log(out)
+  });
+}
