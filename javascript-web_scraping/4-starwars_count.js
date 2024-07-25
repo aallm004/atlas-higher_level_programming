deca0bb82e9a2775abeb65a4c7b33f2a33f6a639
aallm004 <abigailrosea10@gmail.com>
2024-07-25 13:13:16 -0500

    task 4

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -1,11 +1,17 @@
 #!/usr/bin/node
 
-const url = 'https://swapi-api.hbtn.io/api/people/18';
+const url = process.argv[2];
 const request = require('request');
 
+  if (url !== undefined) {
 request(url, (error, response, body) => {
   if (error) console.log(error);
 
-  const data = JSON.parse(body);
-  console.log(data.films.length);
-});
+  const data = JSON.parse(body).results;
+
+  let movie = data.filter(x => x.characters.includes(
+    'https://swapi-api.hbtn.io/api/people/18/'))
+    console.log(movie)
+  console.log(movie.length);
+})
+  }
