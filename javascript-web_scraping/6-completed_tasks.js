164ed4f084935df6d06faeaa60c1e6da97f72f8b
aallm004 <abigailrosea10@gmail.com>
2024-07-25 16:05:23 -0500

    semistandard kind of life

--- javascript-web_scraping/6-completed_tasks.js
+++ javascript-web_scraping/6-completed_tasks.js
@@ -1,19 +1,17 @@
 #!/usr/bin/node
 const url = process.argv[2];
 const request = require('request');
-let out = {}
+const out = {};
+let results;
 
 if (url !== undefined) {
   request(url, (error, response, body) => {
     if (error) console.log(error);
-const data = JSON.parse(body);
-    results = data.filter(x => x.completed)
+    const data = JSON.parse(body);
+    results = data.filter(x => x.completed);
     results.forEach(element => {
-      if (out[element.userId])
-        out[element.userId] += 1
-      else
-        out[element.userId] = 1
-    })
-    console.log(out)
+      if (out[element.userId]) { out[element.userId] += 1; } else { out[element.userId] = 1; }
+    });
+    console.log(out);
   });
 }
